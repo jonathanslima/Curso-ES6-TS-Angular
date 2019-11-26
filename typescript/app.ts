bb0a12d7aeedaeb87be229cd7579b8460f20b16f
Lima <jonathan.da.s.lima@accenture.com>
2019-11-26 17:53:59 -0300

    finalizado regra de negocio no front

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,99 +1,95 @@
-let ola = (nome: string) => {
-  console.log(`Olá ${nome}!`);
-};
-
-ola("Alice de Oliveira Lima!");
-
-let nome: string = "Jonathan";
-let idade: number = 33;
-let homem: boolean = true;
-let interesses: string[] = ["leitura", "desenho", "podcast", "idiomas"];
-let notaInteresses: number[] = [5, 2, 4, 5];
-let arrMixed1: (number | string)[] = [1, 2, 3, "oi"];
-let arrMixed2: [number, number, number, string] = [1, 2, 3, "oi"];
-let obj1: {} = { chave: "valor", key: 2 };
-let obj2: object = { chave: "valor", key: 2 };
-
-interface Person {
-  nome: string;
-  idade: number;
-}
-
-const greeter = (person: Person) => {
-  console.log(`${person.nome} tem ${person.idade} anos!`);
-};
-let jon = { nome: "Jonathan", idade: 33 };
-let alice = { nome: "Alice", idade: 4 };
-let davi = { nome: "Davi", idade: 6 };
-
-greeter(jon);
-greeter(alice);
-greeter(davi);
+// let nome: string = "Jonathan";
+// let idade: number = 33;
+// let homem: boolean = true;
+// let interesses: string[] = ["leitura", "desenho", "podcast", "idiomas"];
+// let notaInteresses: number[] = [5, 2, 4, 5];
+// let arrMixed1: (number | string)[] = [1, 2, 3, "oi"];
+// let arrMixed2: [number, number, number, string] = [1, 2, 3, "oi"];
+// let obj1: {} = { chave: "valor", key: 2 };
+// let obj2: object = { chave: "valor", key: 2 };
 
 class Carro {
-  modelo: string;
-  numeroDePortas: number;
-  velocidade: number;
+  private modelo: string;
+  private numeroDePortas: number;
+  private velocidade: number = 0;
 
-  constructor(Model: string, NPortas: number, Vel: number) {
-    this.modelo = Model;
-    this.numeroDePortas = NPortas;
-    this.velocidade = Vel;
+  constructor(mod: string, nportas: number) {
+    this.modelo = mod;
+    this.numeroDePortas = nportas;
   }
 
-  acelerar(v: number = 0) {
-    return console.log(this.velocidade !== 0);
+  public acelerar(): void {
+    this.velocidade += 10;
   }
-  parar(v: number = 0) {
-    return console.log(this.velocidade === 0);
+
+  public parar(): void {
+    this.velocidade = 0;
   }
-  velocidadeAtual(v: number = 0) {
-    return console.log(this.velocidade);
+
+  public velocidadeAtual(): number {
+    return this.velocidade;
   }
-}
+};
 
 class Concessionaria {
-  endereco: string;
-  listaDeCarros: string[];
+  private endereco: string;
+  private listaDeCarros: any;
 
-  constructor(end: string, lista: string[]) {
+  constructor(end: string, listaDeCarros: Array<Carro>) {
     this.endereco = end;
-    this.listaDeCarros = lista;
+    this.listaDeCarros = listaDeCarros;
   }
 
-  fornecerEndereco() {
-    return console.log(this.endereco);
+  fornecerEndereco(): string {
+    return this.endereco;
   }
 
-  montarListaDeCarros() {
-    return console.log(this.listaDeCarros);
+  montarListaDeCarros(): any {
+    return this.listaDeCarros;
   }
 };
 
 class Pessoa {
-  nome: string;
-  carro: string;
-  carroPreferido: string;
+  private nome: string;
+  private carro: Carro;
+  private carroPreferido: string;
 
-  constructor(n: string, c: string, cp: string){
-    this.nome = n;
-    this.carro = c;
-    this.carroPreferido = cp;
+  constructor(nome: string, carro: any, carroPreferido: string) {
+    this.nome = nome;
+    this.carro = carro;
+    this.carroPreferido = carroPreferido;
   }
 
-  dizerNome(name: string = 'Usuário não identificado'){
-    return console.log(this.nome);
+  public dizerNome() : string {
+    return this.nome;
   }
 
-  dizerCarroPreferido(){
-    return console.log(this.carroPreferido)
+  public dizerCarroPreferido() : string{
+    return this.carroPreferido;
   }
 
-  comprarCarro(){
-    return console.log(this.carro);
+  public comprarCarro(carro: Carro) : void{
+    this.carro = carro;
   }
 
-  dizerCarroQueTem(){
-
+  public dizerCarroQueTem() : Carro{
+    return this.carro;
   }
 };
+
+let carro1 = new Carro('Veloster', 4);
+let carro2 = new Carro('Fusca', 2);
+let carro3 = new Carro('Doblo', 5);
+
+// let listaDeCarros : Carro[] = [carro1, carro2, carro3];
+let listaDeCarros : Array<Carro> = [carro1, carro2, carro3];
+let concessionaria1 = new Concessionaria('Av Rio Branco', listaDeCarros);
+let cliente = new Pessoa('Jonathan', '', 'Fusca');
+
+concessionaria1.montarListaDeCarros().map((carro: Carro)=> {
+  if(carro['modelo'] === cliente.dizerCarroPreferido()){
+    cliente.comprarCarro(carro);
+  }
+})
+
+console.log(cliente.dizerCarroQueTem());
--- typescript/dist/js/app.js
+++ typescript/dist/js/app.js
@@ -1,78 +1,76 @@
 "use strict";
-var ola = function (nome) {
-    console.log("Ol\u00E1 " + nome + "!");
-};
-ola("Alice de Oliveira Lima!");
-var nome = "Jonathan";
-var idade = 33;
-var homem = true;
-var interesses = ["leitura", "desenho", "podcast", "idiomas"];
-var notaInteresses = [5, 2, 4, 5];
-var arrMixed1 = [1, 2, 3, "oi"];
-var arrMixed2 = [1, 2, 3, "oi"];
-var obj1 = { chave: "valor", key: 2 };
-var obj2 = { chave: "valor", key: 2 };
-var greeter = function (person) {
-    console.log(person.nome + " tem " + person.idade + " anos!");
-};
-var jon = { nome: "Jonathan", idade: 33 };
-var alice = { nome: "Alice", idade: 4 };
-var davi = { nome: "Davi", idade: 6 };
-greeter(jon);
-greeter(alice);
-greeter(davi);
+// let nome: string = "Jonathan";
+// let idade: number = 33;
+// let homem: boolean = true;
+// let interesses: string[] = ["leitura", "desenho", "podcast", "idiomas"];
+// let notaInteresses: number[] = [5, 2, 4, 5];
+// let arrMixed1: (number | string)[] = [1, 2, 3, "oi"];
+// let arrMixed2: [number, number, number, string] = [1, 2, 3, "oi"];
+// let obj1: {} = { chave: "valor", key: 2 };
+// let obj2: object = { chave: "valor", key: 2 };
 var Carro = /** @class */ (function () {
-    function Carro(Model, NPortas, Vel) {
-        this.modelo = Model;
-        this.numeroDePortas = NPortas;
-        this.velocidade = Vel;
+    function Carro(mod, nportas) {
+        this.velocidade = 0;
+        this.modelo = mod;
+        this.numeroDePortas = nportas;
     }
-    Carro.prototype.acelerar = function (v) {
-        if (v === void 0) { v = 0; }
-        return console.log(this.velocidade !== 0);
+    Carro.prototype.acelerar = function () {
+        this.velocidade += 10;
     };
-    Carro.prototype.parar = function (v) {
-        if (v === void 0) { v = 0; }
-        return console.log(this.velocidade === 0);
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
     };
-    Carro.prototype.velocidadeAtual = function (v) {
-        if (v === void 0) { v = 0; }
-        return console.log(this.velocidade);
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
     };
     return Carro;
 }());
+;
 var Concessionaria = /** @class */ (function () {
-    function Concessionaria(end, lista) {
+    function Concessionaria(end, listaDeCarros) {
         this.endereco = end;
-        this.listaDeCarros = lista;
+        this.listaDeCarros = listaDeCarros;
     }
     Concessionaria.prototype.fornecerEndereco = function () {
-        return console.log(this.endereco);
+        return this.endereco;
     };
     Concessionaria.prototype.montarListaDeCarros = function () {
-        return console.log(this.listaDeCarros);
+        return this.listaDeCarros;
     };
     return Concessionaria;
 }());
 ;
 var Pessoa = /** @class */ (function () {
-    function Pessoa(n, c, cp) {
-        this.nome = n;
-        this.carro = c;
-        this.carroPreferido = cp;
+    function Pessoa(nome, carro, carroPreferido) {
+        this.nome = nome;
+        this.carro = carro;
+        this.carroPreferido = carroPreferido;
     }
-    Pessoa.prototype.dizerNome = function (name) {
-        if (name === void 0) { name = 'Usuário não identificado'; }
-        return console.log(this.nome);
+    Pessoa.prototype.dizerNome = function () {
+        return this.nome;
     };
     Pessoa.prototype.dizerCarroPreferido = function () {
-        return console.log(this.carroPreferido);
+        return this.carroPreferido;
     };
-    Pessoa.prototype.comprarCarro = function () {
-        return console.log(this.carro);
+    Pessoa.prototype.comprarCarro = function (carro) {
+        this.carro = carro;
     };
     Pessoa.prototype.dizerCarroQueTem = function () {
+        return this.carro;
     };
     return Pessoa;
 }());
 ;
+var carro1 = new Carro('Veloster', 4);
+var carro2 = new Carro('Fusca', 2);
+var carro3 = new Carro('Doblo', 5);
+// let listaDeCarros : Carro[] = [carro1, carro2, carro3];
+var listaDeCarros = [carro1, carro2, carro3];
+var concessionaria1 = new Concessionaria('Av Rio Branco', listaDeCarros);
+var cliente = new Pessoa('Jonathan', '', 'Fusca');
+concessionaria1.montarListaDeCarros().map(function (carro) {
+    if (carro['modelo'] === cliente.dizerCarroPreferido()) {
+        cliente.comprarCarro(carro);
+    }
+});
+console.log(cliente.dizerCarroQueTem());
