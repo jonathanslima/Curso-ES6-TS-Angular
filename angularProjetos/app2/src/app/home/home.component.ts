2f7d739f91e7ddede80eab16814d574dc14b6c24
Lima <jonathan.da.s.lima@accenture.com>
2020-02-10 23:43:14 -0300

    criando serviço pra pegar ofertas

--- angularProjetos/app2/src/app/home/home.component.ts
+++ angularProjetos/app2/src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ofertasService } from '../oferta.service';
+import { oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ofertasService]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public ofertas: oferta[];
+  constructor(private ofertasService: ofertasService) { }
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.GetOfertas();
+    return this.ofertas;
   }
 
 }
