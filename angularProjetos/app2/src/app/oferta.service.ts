aa5b2675ec6a721d22b7b7c9bcb3618ad69e8eb7
Lima <jonathan.da.s.lima@accenture.com>
2020-02-16 23:42:35 -0300

    add filter

--- angularProjetos/app2/src/app/oferta.service.ts
+++ angularProjetos/app2/src/app/oferta.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/toPromise'
+
+// import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class ofertasService{
   constructor(private http: HttpClient){}
 
   public getOfertas(): Promise<oferta[]>{
-    return this.http.get('http://localhost:3000/ofertas')
+    return this.http.get('http://localhost:3000/ofertas?destaque=true')
       .toPromise()
-      .then((res: any) => res.json())
+      .then((res: any) => res)
+      .catch(error => console.error('ERRO: ', error))
   }
 }
