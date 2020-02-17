b9fae1d60c548e6955e861bcdaa36778319689c0
Lima <jonathan.da.s.lima@accenture.com>
2020-02-16 21:09:30 -0300

    add httpClientModule and @Injectable

--- angularProjetos/app2/src/app/oferta.service.ts
+++ angularProjetos/app2/src/app/oferta.service.ts
@@ -1,75 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise'
 
 @Injectable()
 export class ofertasService{
-  constructor(private http: HttpClientModule){}
-
-  public ofertas: Array<oferta> = [
-    {
-      id: 1,
-      categoria: "restaurante",
-      titulo: "Super Burger",
-      descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-      anunciante: "Original Burger",
-      valor: 29.90,
-      destaque: true,
-      imagens: [
-        {url: "/assets/ofertas/1/img1.jpg"},
-        {url: "/assets/ofertas/1/img2.jpg"},
-        {url: "/assets/ofertas/1/img3.jpg"},
-        {url: "/assets/ofertas/1/img4.jpg"}
-      ]
-    },
-    {
-      id: 2,
-      categoria: "restaurante",
-      titulo: "Cozinha Mexicana",
-      descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-      anunciante: "Mexicana",
-      valor: 32.90,
-      destaque: true,
-      imagens: [
-        {url: "/assets/ofertas/2/img1.jpg"},
-        {url: "/assets/ofertas/2/img2.jpg"},
-        {url: "/assets/ofertas/2/img3.jpg"},
-        {url: "/assets/ofertas/2/img4.jpg"}
-      ]
-
-    },
-    {
-      id: 4,
-      categoria: "diversao",
-      titulo: "Estância das águas",
-      descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-      anunciante: "Estância das águas",
-      valor: 31.90,
-      destaque: true,
-      imagens: [
-        {url: "/assets/ofertas/3/img1.jpg"},
-        {url: "/assets/ofertas/3/img2.jpg"},
-        {url: "/assets/ofertas/3/img3.jpg"},
-        {url: "/assets/ofertas/3/img4.jpg"},
-        {url: "/assets/ofertas/3/img5.jpg"},
-        {url: "/assets/ofertas/3/img6.jpg"}
-      ]
-    }
-  ];
-
-  // public GetOfertas(): Array<oferta>{
-  //   return this.ofertas;
-  // }
+  constructor(private http: HttpClient){}
 
   public getOfertas(): Promise<oferta[]>{
-    return new Promise((resolve, reject)=>{
-      resolve(this.ofertas);
-      reject(()=>{
-        return {
-          error: 404,
-          description: 'The request hasn\'t been sent'
-        }
-      })
-    })
+    return this.http.get('http://localhost:3000/ofertas')
+      .toPromise()
+      .then((res: any) => res.json())
   }
 }
