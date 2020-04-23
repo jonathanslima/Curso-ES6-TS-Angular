6cfc0ccf00fddfbe08dc100f004f5e3e712c3811
Lima <jonathan.da.s.lima@accenture.com>
2020-04-22 22:54:00 -0300

    add content in ofertas component

--- angularProjetos/app2/src/app/oferta.service.ts
+++ angularProjetos/app2/src/app/oferta.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { oferta } from './shared/oferta.model';
 import { Injectable } from '@angular/core';
+import { URL_API } from './urls.api';
 
 // import 'rxjs/add/operator/toPromise'
 
@@ -9,16 +10,23 @@ export class ofertasService{
   constructor(private http: HttpClient){}
 
   public getOfertas(): Promise<oferta[]>{
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((res: any) => res)
       .catch(error => console.error('ERRO: ', error))
   }
 
   public getOfertasPorCategoria(categoria: string): Promise<oferta[]>{
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((res: any)=> res)
+      .catch(error => console.error('ERRO: ', error))
+  }
+
+  public getOfertaById(id: any): Promise<oferta[]>{
+    return this.http.get(`${URL_API}?id=${id}`)
+      .toPromise()
+      .then((res: any) => res)
       .catch(error => console.error('ERRO: ', error))
   }
 }
--- angularProjetos/app2/src/app/oferta/oferta.component.ts
+++ angularProjetos/app2/src/app/oferta/oferta.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ofertasService } from '../oferta.service';
+import { oferta } from '../shared/oferta.model'
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.scss']
+  styleUrls: ['./oferta.component.scss'],
+  providers: [ ofertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public ofertas: oferta[];
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: ofertasService
+  ) { }
 
   ngOnInit() {
-  }
+    // console.log('route param: ', this.route.snapshot.params.id)
+    let p: string;
+    
+    this.route.params.subscribe((param: any)=>{
+      p = param.id;
+    })
 
+    this.ofertasService.getOfertaById(p)
+    .then((data: oferta[]) => {
+        console.log(data)
+        this.ofertas = data;
+      })
+  }
 }
